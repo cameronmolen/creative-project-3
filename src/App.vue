c72acd0532de4042cd8ef09a5acbfa7da7c1d1bc
Cameron Molen <cameronmolen@gmail.com>
2021-03-15 16:11:44 -0600

    Added Bootstrap to the project

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,42 @@
 <template>
   <div id="app">
     <div id="nav">
-      <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #264653;">
-        <router-link class="navbar-brand" to="/">Tenacity</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <router-link class="nav-item nav-link" to="/">Home</router-link>
-            <router-link class="nav-item nav-link" to="/">Timeline</router-link>
-            <router-link class="nav-item nav-link" to="/">Tenacity's Story</router-link>
-          </div>
-          <div class="navbar-nav">
-            <nav class="navbar navbar-light" style="padding-left: 0px;">
-              <form class="form-inline">
-                <router-link class="btn btn-outline-success" to="/">Download</router-link>
-              </form>
-            </nav>
-          </div>
-        </div>
-      </nav>
-      <router-link to="/">Tenacity</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-navbar toggleable="sm" type="light" variant="light">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+        <b-navbar-brand>Tenacity</b-navbar-brand>
+
+        <b-collapse id="nav-text-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view/>
+    <footerComponent>
+      <div class="footer">
+        <p>Designed and built by Cameron Molen. Currently work in progress.</p>
+        <a href="https://github.com/cameronmolen/creative-project-3">GitHub</a>
+      </div>
+    </footerComponent>
   </div>
-  <footer>
-    <p>Designed and built by Cameron Molen. Currently work in progress.</p>
-    <a href="https://github.com/cameronmolen/creative-project-3">GitHub</a>
-  </footer>
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+}
+
+a:link, a:visited {
+  text-decoration: none;
+  color: #999999;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -52,5 +56,15 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.footer {
+  width: 90%;
+  justify-content: center;
+  flex-direction: column;
+  color: #999999;
+  padding: 25px 0px;
+  background-color: #EFEFEF;
+  box-shadow: 0 50vh 0 50vh #EFEFEF;
 }
 </style>
