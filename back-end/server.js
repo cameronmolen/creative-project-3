7ab0ecbaa20ba9d8f531e15d02e5aaf4470e790c
Cameron Molen <cameronmolen@gmail.com>
2021-04-01 18:24:00 -0600

    Created schemes and models for feature requests and comments

--- back-end/server.js
+++ back-end/server.js
@@ -13,9 +13,30 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/todo', {
+mongoose.connect('mongodb://localhost:27017/tenacity', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+// Create a scheme for feature requests
+const requestSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  user: String
+});
+
+const Request = mongoose.model('Request', requestSchema);
+
+// Create a scheme for comment responses to feature requests
+const commentSchema = new mongoose.Schema({
+  request: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Request'
+  }
+  user: String,
+  content: String
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
